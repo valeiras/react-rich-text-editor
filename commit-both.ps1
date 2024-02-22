62d3c136f3aae3ca0eaf45ad4a8295b03700204d
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-02-22 18:03:51 +0100

    corrects bad script

--- commit-both.ps1
+++ commit-both.ps1
@@ -1,12 +1,12 @@
-param ($message='Minor updates')
+param ($message = 'Minor updates')
 
 git add -A
 git commit -m $message
 git push -u origin main
 
 npm run build
-Copy-Item ".\assets\draggable-block-menu.svg" -Destination ".\rich-editor"
-Set-Location .\rich-editor\
+Copy-Item ".\assets\draggable-block-menu.svg" -Destination ".\dist"
+Set-Location .\dist\
 
 git add -A
 git commit -m $message
