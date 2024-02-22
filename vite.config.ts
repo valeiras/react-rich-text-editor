506a2807608355ea57ee00b492bf9ad118bd25a1
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-02-22 17:44:37 +0100

    changes to rich editor

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/text-editor",
+  base: "/rich-editor",
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        dir: 'text-editor',
+        dir: 'rich-editor',
         entryFileNames: 'rich-editor-wc.js',
         assetFileNames: 'rich-editor-wc.css',
         chunkFileNames: 'chunk.js',
