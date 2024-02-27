a75f315f334b202f38d2e4c2e67e58ca6d3b778b
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-02-27 12:48:11 +0100

    Corrects unworking font sizes

--- src/toolbar-elements/FontSizeDropDown.tsx
+++ src/toolbar-elements/FontSizeDropDown.tsx
@@ -1,20 +1,11 @@
-import StyleDropDown from './StyleDropDown';
+import StyleDropDown from "./StyleDropDown";
 
 const fontSizes = [...Array(9).keys()]
-  .map((x) => `${(0.2 * x + 0.2).toFixed(1)} rem`)
+  .map((x) => `${(0.2 * x + 0.2).toFixed(1)}rem`)
   .concat([...Array(5).keys()].map((x) => `${(0.5 * x + 2).toFixed(1)}rem`));
 
-const FontSizeDropDown = ({
-  selectionFontSize,
-}: {
-  selectionFontSize: string;
-}): JSX.Element => {
-  return (
-    <StyleDropDown
-      currValue={selectionFontSize}
-      availableValues={fontSizes}
-      propertyName="font-size"
-    />
-  );
+console.log(fontSizes);
+const FontSizeDropDown = ({ selectionFontSize }: { selectionFontSize: string }): JSX.Element => {
+  return <StyleDropDown currValue={selectionFontSize} availableValues={fontSizes} propertyName="font-size" />;
 };
 export default FontSizeDropDown;
